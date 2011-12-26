17131ac2bcfbc05111f078b67a72855b681f5e53
Sebastian Gamboa <me@sagmor.com>
2011-12-26 09:24:24 -0300

    Catch errors and show propper response

--- lib/rack/heartbeat.rb
+++ lib/rack/heartbeat.rb
@@ -1,7 +1,9 @@
 module Rack
   class Heartbeat
-    
-    OK = lambda {  [200, {"Content-Type" => "text/plain"}, ["OK"]] }
+    DEFAULT_BLOCK = lambda { }
+
+    OK = [200, {"Content-Type" => "text/plain"}, ["OK"]]
+    FAIL = [500, {"Content-Type" => "text/plain"}, ["FAILURE"]]
 
     DEFAULTS = {
       :path => '/heartbeat',
@@ -11,12 +13,17 @@ module Rack
     def initialize(app, options = {}, &block)
       @app = app
       @options = DEFAULTS.merge(options)
-      @check = block || OK
+      @check = block || DEFAULT_BLOCK
     end
 
     def call(env)
       if env['REQUEST_METHOD'] == @options[:method] && env['PATH_INFO'] == @options[:path]
-        @check.call
+        begin
+          @check.call
+          OK
+        rescue
+          FAIL
+        end
       else
         @app.call(env)
       end

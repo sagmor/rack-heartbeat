003df75f7eda282f02ed8334efcd1bc92b5c88ec
Sebastian Gamboa <me@sagmor.com>
2011-12-26 09:21:32 -0300

    Allow custom blocks

--- lib/rack/heartbeat.rb
+++ lib/rack/heartbeat.rb
@@ -1,19 +1,22 @@
 module Rack
   class Heartbeat
+    
+    OK = lambda {  [200, {"Content-Type" => "text/plain"}, ["OK"]] }
+
     DEFAULTS = {
       :path => '/heartbeat',
-      :method => 'GET',
-      :response => 'OK'
+      :method => 'GET'
     }
 
-    def initialize(app, options = {})
+    def initialize(app, options = {}, &block)
       @app = app
       @options = DEFAULTS.merge(options)
+      @check = block || OK
     end
 
     def call(env)
       if env['REQUEST_METHOD'] == @options[:method] && env['PATH_INFO'] == @options[:path]
-        [200, {"Content-Type" => "text/plain"}, [@options[:response]]]
+        @check.call
       else
         @app.call(env)
       end

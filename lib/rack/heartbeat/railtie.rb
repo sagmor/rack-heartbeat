61c5bf66713950dfb87157a6ff90673e03c2d135
Sebastian Gamboa <me@sagmor.com>
2011-12-26 09:47:48 -0300

    Yet another app reference missing

--- lib/rack/heartbeat/railtie.rb
+++ lib/rack/heartbeat/railtie.rb
@@ -1,6 +1,6 @@
 module Rack
   class Heartbeat::Railtie < Rails::Railtie
-    initializer "rack.heartbeat.register" do
+    initializer "rack.heartbeat.register" do |app|
       app.middleware.insert 0, Rack::Heartbeat
     end
   end
